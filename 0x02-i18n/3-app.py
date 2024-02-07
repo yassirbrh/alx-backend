9c407787b0108b2219f08bf1dbbeba1611e6c9dd
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-02-07 01:16:03 +0000

    Parameterizing the templates, by using the message IDs home_title and home_header.

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+'''
+    basic Flask app with a single route of /
+'''
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config(object):
+    """docstring for Config"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    '''
+        get_locale: function
+        return: The locale of the request.
+    '''
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index_page() -> str:
+    '''
+        index: function
+        return: the main page
+    '''
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
