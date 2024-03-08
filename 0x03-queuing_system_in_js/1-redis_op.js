57f940dc5f006977636cf1693fe8bfb4382e273e
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-03-08 11:15:12 +0000

    Adding two functions setNewSchool and displaySchoolValue using Callbacks.

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+
+import { createClient, print } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+function setNewSchool(schoolName, value) {
+	client.set(schoolName, value, print);
+}
+
+function displaySchoolValue(schoolName) {
+	client.get(schoolName, (err, reply) => {
+		if (err) {
+			console.log(err);
+		} else {
+			console.log(reply);
+		}
+	})
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
